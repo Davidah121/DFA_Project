9bb3872dc8d08513f84b006df167b3a85f1dbb2f
thodzen <thodzen@gmail.com>
2019-04-02 17:17:58 -0400

    Added stuff
    Stuff includes State and Transition

---
+++ State.cpp
@@ -0,0 +1,49 @@
+#include "State.h"
+
+
+State::State(char name)
+{
+	this->name = name;
+}
+
+char State::getName()
+{
+	return name;
+}
+
+char State::setName(char name)
+{
+	this->name = name;
+}
+
+bool State::isFinalState()
+{
+	return finalState;
+}
+
+void State::setFinalState(bool finalState)
+{
+	this->finalState = finalState;
+}
+
+bool State::isAcceptState()
+{
+	return false;
+}
+
+void State::setAcceptState(bool acceptState)
+{
+	this->acceptState = acceptState;
+}
+
+void State::addTransition(Transition trans)
+{
+	this->trans.push_back(trans);
+}
+
+
+
+State::~State()
+{
+}
+
